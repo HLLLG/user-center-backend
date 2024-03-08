139d61cdce12f17e00215cb76552a5845c966dba
HLLLG <2162803956@qq.com>
2024-03-08 21:49:35 +0800

    伙伴匹配系统后端代码初始化完成

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,21 +1,22 @@
 -- auto-generated definition
-create table user
+create table tag
 (
-    id           bigint auto_increment comment '用户id'
+    id         bigint auto_increment comment 'id'
         primary key,
-    username     varchar(256)                       null comment '昵称',
-    userAccount  varchar(256)                       null comment '登录账号',
-    avatarUrl    varchar(1024)                      null comment '头像',
-    gender       tinyint                            null comment '性别 ',
-    userPassword varchar(512)                       not null comment '密码  ',
-    phone        varchar(128)                       null comment '电话 ',
-    email        varchar(512)                       null comment '邮箱',
-    userStatus   int      default 0                 null comment '用户状态 0 - 正常 ',
-    createTime   datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime   datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint  default 0                 null comment '是否删除',
-    userRole     int      default 0                 not null comment '用户角色 0 - 普通用户 1 - 管理员',
-    planetCode   varchar(256)                       null comment '星球编号'
+    tagName    varchar(256)                       null comment '标签名称',
+    userId     bigint                             null comment '用户 id',
+    parentId   bigint                             null comment '父标签 id',
+    isParent   tinyint                            null comment '0 不是父标签、1 - 父标签',
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint  default 0                 null comment '是否删除',
+    constraint uniIdx_tagName
+        unique (tagName)
 )
-    comment '用户表';
+    comment '标签';
+
+create index idx_userId
+    on tag (userId);
+
+
 
